232f4cf77eb28055987924b0985f7463ac481f50
Aaron Chambers <achambers@gmail.com>
2017-08-17 15:32:26 +0100

    Automatically install Launch Darkly client lib

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-launch-darkly',
 
   isDevelopingAddon() {
     return true;
+  },
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    this.import('vendor/ldclient.js');
+  },
+
+  treeForVendor(vendorTree) {
+    var ldTree = new Funnel(path.dirname(require.resolve('ldclient-js/dist/ldclient.js')), {
+      files: ['ldclient.js'],
+    });
+
+    return new MergeTrees([vendorTree, ldTree]);
   }
 };
