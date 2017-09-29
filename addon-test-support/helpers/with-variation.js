29f0dcfc15996a4c1ac6f21b0795551353075435
Aaron Chambers <achambers@gmail.com>
2017-09-29 14:28:43 +0100

    Change withHelper to use client

--- addon-test-support/helpers/with-variation.js
+++ addon-test-support/helpers/with-variation.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export function withVariation(app, key, value = true) {
-  let launchDarkly = app.__container__.lookup('service:launch-darkly');
-  launchDarkly.variation(key, value);
+  let client = app.__container__.lookup('service:launch-darkly-client');
+  client.setVariation(key, value);
 }
 
 Ember.Test.registerHelper('withVariation', withVariation);
