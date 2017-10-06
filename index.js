9c98d8ce0e3c9ce4791ab340d042a70095601cc8
Aaron Chambers <achambers@gmail.com>
2017-10-06 17:53:22 +0100

    Use config/targets.js to determine whether to include EventSource polyfill

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /* eslint-env node */
 'use strict';
 
-var path = require('path');
-var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const browserslist = require('browserslist');
+
+const EVENT_SOURCE_NON_SUPPORTED_BROWSERS = [
+  'Chrome < 6',
+  'Edge > 0',
+  'Firefox < 6.0',
+  'ie > 0',
+  'Safari < 5'
+];
 
 module.exports = {
   name: 'ember-launch-darkly',
@@ -26,13 +35,44 @@ module.exports = {
     }
 
     this.import('vendor/ldclient.js');
+
+    if (this._shouldIncludePolyfill()) {
+      this.import('vendor/eventsource.js');
+    }
   },
 
   treeForVendor(vendorTree) {
-    var ldTree = new Funnel(path.dirname(require.resolve('ldclient-js/dist/ldclient.js')), {
+    let trees = vendorTree ? [vendorTree] : [];
+
+    trees.push(this._launchDarklyTree());
+
+    if (this._shouldIncludePolyfill()) {
+      trees.push(this._eventSourceTree());
+    }
+
+    return new MergeTrees(trees);
+  },
+
+  _launchDarklyTree() {
+    return new Funnel(path.dirname(require.resolve('ldclient-js/dist/ldclient.js')), {
       files: ['ldclient.js'],
     });
+  },
 
-    return vendorTree ? new MergeTrees([vendorTree, ldTree]) : ldTree;
+  _eventSourceTree() {
+    return new Funnel(path.dirname(require.resolve('event-source-polyfill/eventsource.js')), {
+      files: ['eventsource.js'],
+    });
+  },
+
+  _shouldIncludePolyfill() {
+    if (this.project.targets && this.project.targets.browsers) {
+      let browsers = browserslist(this.project.targets.browsers);
+      let prohibitedBrowsers = browserslist(EVENT_SOURCE_NON_SUPPORTED_BROWSERS);
+
+      return prohibitedBrowsers.filter(version => browsers.includes(version)).length;
+    }
+
+    return false;
   }
 };
