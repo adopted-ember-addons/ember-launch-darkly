dde9f7f93e3d5dbdb48e4b409854498ffc950ccc
Jared Galanis <jaredgalanis@gmail.com>
2020-09-04 15:24:37 -0400

    update client to use new import style - removes dead code path for LDClient on window - uses imported initialize directly rather than calling it on the client

--- addon/services/launch-darkly-client-remote.js
+++ addon/services/launch-darkly-client-remote.js
@@ -39,14 +39,6 @@ export default Service.extend(Evented, {
       return RSVP.resolve();
     }
 
-    if (!LDClient) {
-      warn('Launch Darkly JS client not found. Defaulting all feature flags to "false"', false, { id: 'ember-launch-darkly.client-not-found' });
-
-      this.set('_client', NullClient);
-
-      return RSVP.resolve();
-    }
-
     return this._initialize(clientSideId, user, streaming);
   },
 
@@ -70,7 +62,7 @@ export default Service.extend(Evented, {
 
   _initialize(id, user, streamingOptions) {
     return new RSVP.Promise((resolve, reject) => {
-      let client = LDClient.initialize(id, user);
+      let client = initialize(id, user);
 
       client.on('ready', () => {
         this.set('_client', client);
