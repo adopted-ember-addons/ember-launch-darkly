16e6ad045f6d0b371c60b437409eea48e6a763d6
Aaron Chambers <aaron.chambers@hey.com>
2020-10-27 14:42:12 +0000

    WIP Fix template lint

--- tests/acceptance/variations-test.js
+++ tests/acceptance/variations-test.js
@@ -14,19 +14,54 @@ module('Acceptance | variations', function(hooks) {
     await visit('/acceptance-tests');
 
     assert.dom('.flag-value').hasText('false', 'Feature flag is disabled');
-    assert.dom('.template-if-statement').hasText('FOO', 'Test variation helper in template "if" statement');
-    assert.dom('.template-with-statement').hasText('FOO -', 'Test variation helper in template "with" statement');
-    assert.dom('.template-let-statement').hasText('FOO', 'Test variation helper in template "let" statement');
-    assert.dom('.single-variation-computed').hasText('FOO', 'Test single variation in computed property in controller');
-    assert.dom('.multiple-variation-computed').hasText('FOO', 'Test multiiple variations in computed property in controller');
+    assert
+      .dom('.template-if-statement')
+      .hasText('FOO', 'Test variation helper in template "if" statement');
+    assert
+      .dom('.template-with-statement')
+      .hasText(
+        'FOO - 💥',
+        'Test variation helper in template "with" statement'
+      );
+    assert
+      .dom('.template-let-statement')
+      .hasText('FOO', 'Test variation helper in template "let" statement');
+    assert
+      .dom('.single-variation-computed')
+      .hasText(
+        'FOO',
+        'Test single variation in computed property in controller'
+      );
+    assert
+      .dom('.multiple-variation-computed')
+      .hasText(
+        'FOO',
+        'Test multiiple variations in computed property in controller'
+      );
 
     await click('.toggle');
 
     assert.dom('.flag-value').hasText('true', 'Feature flag is enabled');
-    assert.dom('.template-if-statement').hasText('BAR', 'Test variation helper in template "if" statement');
-    assert.dom('.template-with-statement').hasText('BAR - true', 'Test variation helper in template "with" statement');
-    assert.dom('.template-let-statement').hasText('BAR', 'Test variation helper in template "let" statement');
-    assert.dom('.single-variation-computed').hasText('BAR', 'Test single computed property in controller');
-    assert.dom('.multiple-variation-computed').hasText('BAR', 'Test multiple variations in  computed property in controller');
+    assert
+      .dom('.template-if-statement')
+      .hasText('BAR', 'Test variation helper in template "if" statement');
+    assert
+      .dom('.template-with-statement')
+      .hasText(
+        'BAR - true',
+        'Test variation helper in template "with" statement'
+      );
+    assert
+      .dom('.template-let-statement')
+      .hasText('BAR', 'Test variation helper in template "let" statement');
+    assert
+      .dom('.single-variation-computed')
+      .hasText('BAR', 'Test single computed property in controller');
+    assert
+      .dom('.multiple-variation-computed')
+      .hasText(
+        'BAR',
+        'Test multiple variations in  computed property in controller'
+      );
   });
 });
