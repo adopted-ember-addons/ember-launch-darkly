631f3b18845bf68afca1c650d0178f707f66a1bd
Aaron Chambers <aaron.chambers@hey.com>
2020-11-03 21:48:23 +0000

    WIP

--- addon/utils/-sdk.js
+++ addon/utils/-sdk.js
@@ -1,5 +1,28 @@
 import * as LDClient from 'launchdarkly-js-client-sdk';
-import * as common from 'launchdarkly-js-sdk-common';
+import { TrackedMap } from 'tracked-built-ins';
+
+function setCurrentContext(context) {
+  window.__LD__ = context;
+}
+
+function getCurrentContext() {
+  return window.__LD__;
+}
+
+class Context {
+  flags = new TrackedMap();
+  client = null;
+
+  constructor(client, flags = {}) {
+    this.client = client;
+
+    this.updateFlags(flags);
+  }
+
+  updateFlags(flags) {
+    Object.entries(flags).forEach(([key, value]) => this.flags.set(key, value));
+  }
+}
 
 async function initialize(
   clientSideId,
@@ -7,15 +30,47 @@ async function initialize(
   options = {},
   ldClient = LDClient
 ) {
-  let client = ldClient.initialize(clientSideId, user, options);
-
-  try {
-    await client.waitForInitialization();
-  } catch (e) {
-    debugger;
+  let context = getCurrentContext();
+  if (context) {
+    return;
   }
 
-  return true;
+  options = {
+    sendEventsOnlyForVariation: true,
+    ...options
+  };
+
+  let client = ldClient.initialize(clientSideId, user, options);
+
+  await client.waitForInitialization();
+
+  let flags = client.allFlags();
+
+  context = new Context(client, flags);
+
+  client.on('change', updates => {
+    let flags = {};
+    Object.entries(updates).forEach(([key, { current, previous }]) => {
+      if (current !== previous) {
+        flags[key] = current;
+      }
+    });
+
+    context.updateFlags(flags);
+  });
+
+  setCurrentContext(context);
 }
 
-export { initialize };
+function variation(key) {
+  let context = getCurrentContext();
+  if (!context) {
+    throw new Error('LD not initialized');
+  }
+
+  context.client.variation(key);
+
+  return context.flags.get(key);
+}
+
+export { initialize, variation };
--- tests/unit/utils/-sdk-test.js
+++ tests/unit/utils/-sdk-test.js
@@ -5,13 +5,52 @@ import { initialize } from 'ember-launch-darkly/utils/-sdk';
 module('Unit | Utility | sdk', function() {
   module('#initialize', function() {
     test('throws an error when clientSideId is not provided', async function(assert) {
-      assert.expect(1);
+      assert.expect(2);
 
       try {
         await initialize();
       } catch (e) {
-        debugger;
-        assert.equal(e.message, 'FOOO');
+        assert.equal(
+          e.constructor.prototype.name,
+          'LaunchDarklyInvalidEnvironmentIdError'
+        );
+
+        assert.equal(
+          e.message,
+          'No environment/client-side ID was specified. Please see https://docs.launchdarkly.com/docs/js-sdk-reference#section-initializing-the-client for instructions on SDK initialization.'
+        );
+      }
+    });
+
+    test('throws an error if invalid user isprovided', async function(assert) {
+      assert.expect(2);
+      try {
+        await initialize('cheese');
+      } catch (e) {
+        assert.equal(
+          e.constructor.prototype.name,
+          'LaunchDarklyInvalidUserError'
+        );
+
+        assert.equal(
+          e.message,
+          'Invalid user specified. Please see https://docs.launchdarkly.com/docs/js-sdk-reference#section-initializing-the-client for instructions on SDK initialization.'
+        );
+      }
+    });
+
+    test('Error experienced when fetching flag settings', async function(assert) {
+      assert.expect(2);
+
+      try {
+        await initialize('invalid-client-side-id', { key: 'blah' });
+      } catch (e) {
+        assert.equal(
+          e.constructor.prototype.name,
+          'LaunchDarklyFlagFetchError'
+        );
+
+        assert.equal(e.message, 'Error fetching flag settings: 400');
       }
     });
   });
