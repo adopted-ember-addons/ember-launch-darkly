dcc513bc056e5397cba58e9c7a157588a46ebba1
Aaron Chambers <aaron.chambers@hey.com>
2020-11-05 14:52:47 +0000

    WIP Add eventsource polyfill

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const browserslist = require('browserslist');
+
+const EVENT_SOURCE_NON_SUPPORTED_BROWSERS = [
+  'Chrome < 6',
+  'Edge > 0',
+  'Firefox < 6.0',
+  'ie > 0',
+  'Safari < 5'
+];
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  isDevelopingAddon() {
+    return true;
+  },
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    if (this._shouldIncludePolyfill()) {
+      this.import('vendor/eventsource.js');
+    }
+  },
+
+  treeForVendor(vendorTree) {
+    let trees = vendorTree ? [vendorTree] : [];
+
+    if (this._shouldIncludePolyfill()) {
+      trees.push(this._eventSourceTree());
+    }
+
+    return new MergeTrees(trees);
+  },
+
+  _eventSourceTree() {
+    return new Funnel(
+      path.dirname(require.resolve('event-source-polyfill/src/eventsource.js')),
+      {
+        files: ['eventsource.js']
+      }
+    );
+  },
+
+  _shouldIncludePolyfill() {
+    if (this.project.targets && this.project.targets.browsers) {
+      let browsers = browserslist(this.project.targets.browsers);
+      let prohibitedBrowsers = browserslist(
+        EVENT_SOURCE_NON_SUPPORTED_BROWSERS
+      );
+
+      return prohibitedBrowsers.filter(version => browsers.includes(version))
+        .length;
+    }
+
+    return false;
+  }
 };
