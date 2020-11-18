06aa2f105c79ffe214f4a4792ba31b3793ced5fb
Aaron Chambers <aaron.chambers@hey.com>
2020-11-18 20:15:27 +0000

    ♻️ Refactor variation template helper to use new variation function

--- addon/helpers/variation.js
+++ addon/helpers/variation.js
@@ -1,40 +1,8 @@
-import Helper from '@ember/component/helper';
-import { inject as service } from '@ember/service';
-import { join } from '@ember/runloop';
-import { buildWaiter } from 'ember-test-waiters';
+import { helper } from '@ember/component/helper';
+import { variation } from 'ember-launch-darkly/-sdk/variation';
 
-let recomputeWaiter = buildWaiter('recompute-waiter');
+function variationHelper([key], { defaultValue = null }) {
+  return variation(key, defaultValue);
+}
 
-export default Helper.extend({
-  launchDarkly: service(),
-
-  recompute() {
-    let token = recomputeWaiter.beginAsync();
-
-    this._super();
-
-    join(() => recomputeWaiter.endAsync(token));
-  },
-
-  compute([key]) {
-    let service = this.launchDarkly;
-
-    if (!this._key) {
-      this._key = key;
-      // eslint-disable-next-line ember/no-observers
-      service.addObserver(key, this, 'recompute');
-    }
-
-    return service.get(key);
-  },
-
-  destroy() {
-    let service = this.launchDarkly;
-
-    if (this._key) {
-      service.removeObserver(this._key, this, 'recompute');
-    }
-
-    return this._super();
-  }
-});
+export default helper(variationHelper);
