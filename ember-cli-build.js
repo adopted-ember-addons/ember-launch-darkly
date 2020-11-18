82a48d787cb29ad2513b54955572efe93f66b680
Aaron Chambers <aaron.chambers@hey.com>
2020-11-18 20:21:30 +0000

    🔥 Remove variation babel plugin

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,10 +4,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    babel: {
-      plugins: [ require.resolve('./babel-plugin') ]
-    },
-
     snippetSearchPaths: ['app', 'tests/dummy'],
     'ember-cli-babel': {
       includePolyfill: true
